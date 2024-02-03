7f6ea80407aab26c58a4f0f2d442b0edb0668566
Dimas <dimasriat@gmail.com>
2024-02-03 12:29:16 +0700

    feat: keymap for split + format

--- lua/gecko2/init.lua
+++ lua/gecko2/init.lua
@@ -5,15 +5,24 @@ local curl = require("plenary.curl")
 local function fetch_coingecko_coins_list()
     -- local req_url = "https://api.coingecko.com/api/v3/coins/list"
     local req_url = "https://api.coingecko.com/api/v3/ping"
-    local response = curl.request{
-        url=req_url,
-        method="get",
-        accept="application/json"
+    local response = curl.request {
+        url = req_url,
+        method = "get",
+        accept = "application/json"
     }
     if response.status ~= 200 then
         error("Could not make request")
     end
     return response.body
+end
+
+local function create_split_buffer(line)
+    vim.cmd('vsplit')
+    local win = vim.api.nvim_get_current_win()
+    local buf = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_win_set_buf(win, buf)
+    vim.api.nvim_buf_set_lines(buf, 0, 1, false, { line })
+    return win, buf
 end
 
 -------------------- Main --------------------
@@ -22,7 +31,7 @@ local M = {}
 
 function M.setup(opts)
     require('plenary.reload').reload_module('gecko2', true)
-    vim.api.nvim_set_keymap('n', '<leader>zs', '<cmd>lua require("gecko").split()<CR>',
+    vim.api.nvim_set_keymap('n', '<leader>zs', '<cmd>lua require("gecko2").fetch_api()<CR>',
         { noremap = true, silent = true })
 end
 
@@ -32,15 +41,8 @@ end
 
 -- lua require("gecko2").fetch_api()
 function M.fetch_api()
-    local response = fetch_coingecko_coins_list()
-    print("hello world gm")
-
-    print("Response as string:")
-    print(response)
-
-    print("Response as JSON:")
-    local response_decoded = vim.fn.json_decode(response)
-    print(response_decoded['gecko_says'])
+    local json_str = fetch_coingecko_coins_list()
+    create_split_buffer(json_str)
 end
 
 function M.split()
@@ -48,10 +50,9 @@ function M.split()
     local win = vim.api.nvim_get_current_win()
     local buf = vim.api.nvim_create_buf(true, true)
     vim.api.nvim_win_set_buf(win, buf)
-    vim.api.nvim_buf_set_lines(buf, 0, 1, false, {"Hello, world!"})
+    vim.api.nvim_buf_set_lines(buf, 0, 1, false, { "Hello, world!" })
 end
 
 M.setup({})
 
 return M
-
