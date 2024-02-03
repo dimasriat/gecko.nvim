cbbed7e6b8fc689f3fc705aac04fe6b14be90bb3
Dimas <dimasriat@gmail.com>
2024-02-03 19:16:18 +0700

    chore: move gecko2 to gecko

--- lua/gecko/init.lua
+++ lua/gecko/init.lua
@@ -1,9 +1,53 @@
-local gecko_ui = require('gecko.ui')
+local curl = require("plenary.curl")
+
+-------------------- Module Helpers --------------------
+
+local function fetch_coingecko_coins_list()
+    local req_url = "https://api.coingecko.com/api/v3/coins/list?include_platform=true"
+    local response = curl.request {
+        url = req_url,
+        method = "get",
+        accept = "application/json"
+    }
+    if response.status ~= 200 then
+        error("Could not make request")
+    end
+    return response.body
+end
+
+local function create_split_buffer(lines)
+    vim.cmd('vsplit')
+    local win = vim.api.nvim_get_current_win()
+    local buf = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_win_set_buf(win, buf)
+    vim.api.nvim_buf_set_lines(buf, 0, #lines, false, lines)
+    return win, buf
+end
+
+-------------------- Main --------------------
 
 local M = {}
 
+function M.setup(opts)
+    require('plenary.reload').reload_module('gecko2', true)
+    vim.api.nvim_set_keymap('n', '<leader>zs', '<cmd>lua require("gecko2").fetch_api()<CR>',
+        { noremap = true, silent = true })
+end
+
 function M.hello()
     print("Wen lambo?")
+end
+
+-- lua require("gecko2").fetch_api()
+function M.fetch_api()
+    local response = fetch_coingecko_coins_list()
+
+    local lines = {}
+    local response_decoded = vim.fn.json_decode(response)
+    for _, coin in ipairs(response_decoded) do
+        table.insert(lines, vim.fn.json_encode(coin))
+    end
+    create_split_buffer(lines)
 end
 
 function M.split()
@@ -11,32 +55,9 @@ function M.split()
     local win = vim.api.nvim_get_current_win()
     local buf = vim.api.nvim_create_buf(true, true)
     vim.api.nvim_win_set_buf(win, buf)
-    vim.api.nvim_buf_set_lines(buf, 0, 1, false, {"Hello, world!"})
+    vim.api.nvim_buf_set_lines(buf, 0, 1, false, { "Hello, world!" })
 end
 
-function M.toggle()
-    gecko_ui.toggle_window()
-end
-
-function M.setup(opts)
-    local function set_default(opt, default)
-        if vim.g["gecko_" .. opt] ~= nil then
-            return
-        elseif opts[opt] ~= nil then
-            vim.g["gecko_" .. opt] = opts[opt]
-        else
-            vim.g["gecko_" .. opt] = default
-        end
-    end
-
-    set_default("window_width", 60)
-    set_default("window_height", 10)
-
-    vim.api.nvim_set_keymap('n', '<leader>zs', '<cmd>lua require("gecko").split()<CR>',
-        { noremap = true, silent = true })
-end
-
--- Default config setup
 M.setup({})
 
 return M
