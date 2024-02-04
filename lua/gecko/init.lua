124e4c42a5b7e332aa4a596d4c2bbd9114f374ad
Dimas <dimasriat@gmail.com>
2024-02-04 09:26:37 +0700

    fix: telescope integration

--- lua/gecko/init.lua
+++ lua/gecko/init.lua
@@ -1,12 +1,15 @@
+local telescope = require('telescope')
+
 local M = {}
 
 function M.setup(opts)
-    require('plenary.reload').reload_module('gecko', true)
+    telescope.load_extension('gecko')
     vim.api.nvim_set_keymap('n', '<leader>zs', '<cmd>lua require("gecko").hello()<CR>',
         { noremap = true, silent = true })
 end
 
 function M.hello()
+    require('plenary.reload').reload_module('gecko', true)
     print("Wen lambo?")
 end
 
