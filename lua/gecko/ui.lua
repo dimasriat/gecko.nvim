391afbcaf0d6a2fba8fbef302198694f099db08b
Dimas <dimasriat@gmail.com>
2024-02-07 21:55:33 +0700

    chore: ui

--- lua/gecko/ui.lua
+++ lua/gecko/ui.lua
@@ -15,12 +15,25 @@ end
 
 function Ui:toggle_ui()
     if self.window_id == nil and self.buffer_id == nil then
+        local buffer_id, window_id = self:create_ui()
+
+        self.buffer_id = buffer_id
+        self.window_id = window_id
+        self.is_window_open = true
+
+        vim.api.nvim_win_set_buf(self.window_id, self.buffer_id)
+        return
+    end
+    self:close_ui()
+end
+
+function Ui:create_ui()
         local width = 160
         local height = 40
         local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
 
-        self.buffer_id = vim.api.nvim_create_buf(false, true)
-        local window_id, _ = popup.create(self.buffer_id, {
+        local buffer_id = vim.api.nvim_create_buf(false, true)
+        local window_id, _ = popup.create(buffer_id, {
             title = "Results",
             line = math.floor(((vim.o.lines - height) / 2)),
             col = math.floor((vim.o.columns - width) / 2),
@@ -28,25 +41,19 @@ function Ui:toggle_ui()
             minheight = height,
             borderchars = borderchars,
         })
-        self.window_id = window_id
-        vim.api.nvim_win_set_buf(self.window_id, self.buffer_id)
-        self.is_window_open = true
 
-        return
-    end
-    self:close_ui()
+        return buffer_id, window_id
 end
 
 function Ui:close_ui()
-    if self.window_id == nil and self.buffer_id == nil then
-        return
+    if self.window_id ~= nil then
+        vim.api.nvim_win_close(self.window_id, true)
+        self.window_id = nil
     end
-    vim.api.nvim_win_close(self.window_id, true)
-    vim.api.nvim_buf_delete(self.buffer_id, { force = true })
-
-    self.window_id = nil
-    self.buffer_id = nil
-
+    if self.buffer_id ~= nil then
+        vim.api.nvim_buf_delete(self.buffer_id, { force = true })
+        self.buffer_id = nil
+    end
     self.is_window_open = false
 end
 
--- lua/gecko/ui_spec.lua
+++ lua/gecko/ui_spec.lua
@@ -1,4 +1,3 @@
-local Ui = require("gecko.ui")
 local gecko = require("gecko")
 
 local eq = assert.are.same
@@ -6,29 +5,74 @@ local eq = assert.are.same
 describe("Ui", function()
     describe("toggle_ui", function()
         it("should toggle the window and buffer", function()
-            local ui = Ui:new()
-            ui:toggle_ui()
+            gecko.ui:toggle_ui()
 
-            local window_id = ui:get_window_id()
-            local buffer_id = ui:get_buffer_id()
+            local window_id = gecko.ui:get_window_id()
+            local buffer_id = gecko.ui:get_buffer_id()
 
             eq(vim.api.nvim_win_is_valid(window_id), true)
             eq(vim.api.nvim_buf_is_valid(buffer_id), true)
-            eq(ui:get_is_window_open(), true)
+            eq(gecko.ui:get_is_window_open(), true)
 
-            ui:toggle_ui()
+            gecko.ui:toggle_ui()
+
             eq(vim.api.nvim_win_is_valid(window_id), false)
             eq(vim.api.nvim_buf_is_valid(buffer_id), false)
-            eq(ui:get_is_window_open(), false)
+            eq(gecko.ui:get_is_window_open(), false)
         end)
 
         it("should close when key pressed `q`", function ()
+            gecko.ui:toggle_ui()
+
+            local window_id = gecko.ui:get_window_id()
+            local buffer_id = gecko.ui:get_buffer_id()
+
+            eq(vim.api.nvim_win_is_valid(window_id), true)
+            eq(vim.api.nvim_buf_is_valid(buffer_id), true)
+            eq(gecko.ui:get_is_window_open(), true)
+
+            -- press q
+            vim.api.nvim_input("q")
+
+            eq(vim.api.nvim_win_is_valid(window_id), false)
+            eq(vim.api.nvim_buf_is_valid(buffer_id), false)
+            eq(gecko.ui:get_is_window_open(), false)
         end)
 
         it("should close when key pressed `esc`", function ()
+            gecko.ui:toggle_ui()
+
+            local window_id = gecko.ui:get_window_id()
+            local buffer_id = gecko.ui:get_buffer_id()
+
+            eq(vim.api.nvim_win_is_valid(window_id), true)
+            eq(vim.api.nvim_buf_is_valid(buffer_id), true)
+            eq(gecko.ui:get_is_window_open(), true)
+
+            -- press esc
+            vim.api.nvim_input("<esc>")
+
+            eq(vim.api.nvim_win_is_valid(window_id), false)
+            eq(vim.api.nvim_buf_is_valid(buffer_id), false)
+            eq(gecko.ui:get_is_window_open(), false)
         end)
 
         it("should close when type :q", function ()
+            gecko.ui:toggle_ui()
+
+            local window_id = gecko.ui:get_window_id()
+            local buffer_id = gecko.ui:get_buffer_id()
+
+            eq(vim.api.nvim_win_is_valid(window_id), true)
+            eq(vim.api.nvim_buf_is_valid(buffer_id), true)
+            eq(gecko.ui:get_is_window_open(), true)
+
+            -- type :q
+            vim.api.nvim_input(":q<cr>")
+
+            eq(vim.api.nvim_win_is_valid(window_id), false)
+            eq(vim.api.nvim_buf_is_valid(buffer_id), false)
+            eq(gecko.ui:get_is_window_open(), false)
         end)
     end)
 end)
